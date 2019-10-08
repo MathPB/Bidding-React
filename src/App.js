bf2ed97a7bb5d8890f2c19570889c41da139793f
MathPB <matheus.pereira4025@hotmail.com>
2019-10-08 19:21:06 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import web3 from './web3';
 
-function App() {
+class App extends Component {
+  render(){
+    // window.ethereum.enable()
+    //   .then(web3.eth.getAccounts()
+    //     .then(console.log));
+
+    web3.eth.getAccounts().then(console.log);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Olá, seu gay</div>
   );
-}
-
+}};
 export default App;
